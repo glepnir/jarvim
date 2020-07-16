fa1200017508d3e7161c6e12fe60573aabe27f93
Raphael <glepnir@gopherhub.org>
2020-07-17 00:13:19 +0800

    update

--- pkg/color/color.go
+++ pkg/color/color.go
@@ -2,27 +2,29 @@ package color
 
 import "fmt"
 
-var Reset = "\033[0m"
-var Red = "\033[31m"
-var Green = "\033[32m"
-var Yellow = "\033[33m"
-var Blue = "\033[34m"
-var Purple = "\033[35m"
-var Cyan = "\033[36m"
-var Gray = "\033[37m"
-var White = "\033[97m"
+var (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	Gray   = "\033[37m"
+	White  = "\033[97m"
+)
 
-// Print Error Message use red color
+// PrintError Message use red color
 func PrintError(message string) {
 	fmt.Println(Red + message + Reset)
 }
 
-// Print Warn Message use yellow color
+// PrintWarn Message use yellow color
 func PrintWarn(message string) {
 	fmt.Println(Yellow + message + Reset)
 }
 
-// Print Success Message use green color
+// PrintSuccess Message use green color
 func PrintSuccess(message string) {
 	fmt.Println(Green + message + Reset)
 }
