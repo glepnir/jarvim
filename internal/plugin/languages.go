0458e1e2958af805da715089f5c2c0f2a599165c
Raphael <glepnir@gopherhub.org>
2020-07-21 00:36:29 +0800

    update

--- internal/plugin/languages.go
+++ internal/plugin/languages.go
@@ -6,46 +6,37 @@ const (
 [[pluings]]
 repo = 'jackguo380/vim-lsp-cxx-highlight'
 on_ft = [ 'c','cpp' ]
-hook_add = '''
-	" ccls config
-	if dein#tap('vim-lsp-cxx-highlight')
-		call coc#config('languageserver', {
-		\ 'ccls': {
-			\ "command": "ccls",
-			\ "rootPatterns": [".ccls", "compile_commands.json", ".git/", ".hg/"],
-			\ "filetypes": ["c","cpp","objc","objcpp"],
-			\ "initializationOptions": {
-			\ "cache":{
-				\ "directory": "/tmp/ccls"
-			\ }
-			\ }
-			\ }
-			\})
-	endif
+hook_source = '''
+	call coc#config('languageserver', {
+	\ 'ccls': {
+		\ "command": "ccls",
+		\ "rootPatterns": [".ccls", "compile_commands.json", ".git/", ".hg/"],
+		\ "filetypes": ["c","cpp","objc","objcpp"],
+		\ "initializationOptions": {
+		\ "cache":{
+			\ "directory": "/tmp/ccls"
+		\ }
+		\ }
+		\ }
+		\})
 '''
 `
 	DeinR = `
 [[plugins]]
 repo = 'jalvesaq/Nvim-R'
 on_ft = 'R'
-hook_add = '''
-	"R lsp config
-	if dein#tap('Nvim-R')
-		call coc#add_extension('coc-R')
-	endif
+hook_source = '''
+	call coc#add_extension('coc-R')
 '''
 `
 	DeinJavascript = `
 [[plugins]]
 repo = 'pangloss/vim-javascript'
 on_ft = [ 'javascript', 'javascriptreact' ]
-hook_add = '''
+hook_source = '''
 	let g:javascript_plugin_jsdoc = 1
     let g:javascript_plugin_flow = 1
-	"javascript lsp config
-	if dein#tap('vim-javascript')
-		call coc#add_extension('coc-tsserver','coc-eslint','coc-prettier','coc-docthis')
-	endif
+	call coc#add_extension('coc-tsserver','coc-eslint','coc-prettier','coc-docthis')
 '''
 
 [[plugins]]
@@ -57,10 +48,8 @@ on_ft = [ 'javascript', 'javascriptreact' ]
 [[plugin]]
 repo = 'HerringtonDarkholme/yats.vim'
 on_ft = [ 'typescript', 'typescriptreact' ]
-hook_add = '''
-	if dein#tap('yats.vim')
-		call coc#add_extension('coc-tsserver','coc-eslint', 'coc-prettier', 'coc-tslint-plugin' ,'coc-docthis')
-	endif
+hook_source = '''
+	call coc#add_extension('coc-tsserver','coc-eslint', 'coc-prettier', 'coc-tslint-plugin' ,'coc-docthis')
 '''
 `
 
@@ -82,17 +71,15 @@ on_ft = 'vue'
 [[plugins]]
 repo = 'hardcoreplayers/go-nvim'
 on_ft = ['go','gomod']
-hook_add = '''
-	if dein#tap('go-nvim')
-		call coc#config('languageserver', {
-		\ 'golang': {
-			\ "command": "gopls",
-			\ "rootPatterns": ["go.mod"],
-			\ "disableWorkspaceFolders": "true",
-			\ "filetypes": ["go"]
-			\ }
-			\})
-	endif
+hook_source = '''
+	call coc#config('languageserver', {
+	\ 'golang': {
+		\ "command": "gopls",
+		\ "rootPatterns": ["go.mod"],
+		\ "disableWorkspaceFolders": "true",
+		\ "filetypes": ["go"]
+		\ }
+		\})
 '''
 `
 
@@ -100,6 +87,9 @@ hook_add = '''
 [[plugins]]
 repo = 'rust-lang/rust.vim'
 on_ft = 'rust'
+hook_source = '''
+	call coc#add_extension('coc-rust-analyzer')
+'''
 `
 	DeinHaskell = `
 [[plugins]]
@@ -111,31 +101,26 @@ on_ft = 'haskell'
 [[plugins]]
 repo = 'StanAngeloff/php.vim'
 on_ft = 'php'
-hook_add = '''
+hook_source = '''
 	"php lsp config
-	if dein#tap('php.vim')
-		call coc#config('languageserver', {
-		\ 'intelephense': {
-			\ "command": "intelephense",
-			\ "args": ["--stdio"],
-			\ "filetypes": ["php"],
-			\ "initializationOptions": {
-				\ "storagePath": "/tmp/intelephense"
-			\ }
-			\ }
-			\})
-	endif
+	call coc#config('languageserver', {
+	\ 'intelephense': {
+		\ "command": "intelephense",
+		\ "args": ["--stdio"],
+		\ "filetypes": ["php"],
+		\ "initializationOptions": {
+			\ "storagePath": "/tmp/intelephense"
+		\ }
+		\ }
+		\})
 '''
 `
 	DeinRuby = `
 [[plugins]]
 repo = 'vim-ruby/vim-ruby'
 on_ft = 'ruby'
-hook_add = '''
-	"Ruby lsp config
-	if dein#tap('vim-ruby')
-		call coc#add_extension('coc-solargraph')
-	endif
+hook_source = '''
+	call coc#add_extension('coc-solargraph')
 '''
 `
 
@@ -143,47 +128,39 @@ hook_add = '''
 [[plugins]]
 repo = 'derekwyatt/vim-scala'
 on_ft = 'scala'
-hook_add = '''
-	"Scala lsp config
-	if dein#tap('vim-scala')
-		call coc#add_extension('coc-metals')
-	endif
+hook_source = '''
+	call coc#add_extension('coc-metals')
 '''
 `
 	DeinShell = `
 [[plugins]]
 repo = 'arzg/vim-sh'
 on_ft = [ 'sh','zsh' ]
-hook_add = '''
-	"shell lsp config
-	if dein#tap('vim-sh')
-		call coc#config('languageserver', {
-		\ 'bash': {
-			\ "command": "bash-language-server",
-			\ "args" : ["start"],
-			\ "ignoredRootPaths": ["~"],
-			\ "filetypes": ["sh"]
-			\ }
-			\})
-	endif
+hook_source = '''
+	call coc#config('languageserver', {
+	\ 'bash': {
+		\ "command": "bash-language-server",
+		\ "args" : ["start"],
+		\ "ignoredRootPaths": ["~"],
+		\ "filetypes": ["sh"]
+		\ }
+		\})
 '''
 `
 	DeinLua = `
 [[plugins]]
 repo = 'tbastos/vim-lua'
 on_ft = 'lua'
-hook_add = '''
-	if dein#tap(vim-lua)
-		call coc#config ('languageserver', {
-				\'lua': {
-					\ "cwd": "full path of lua-language-server directory", (not sure this one is really necessary)
-					\ "command": "full path to lua-language-server executable",
-					\ "args": ["-E", "-e", "LANG=en", "[full path of lua-language-server directory]/main.lua"],
-					\ "filetypes": ["lua"],
-					\ "rootPatterns": [".git/"]
-					\}
-					\})
-	endif
+hook_source = '''
+	call coc#config ('languageserver', {
+			\'lua': {
+				\ "cwd": "full path of lua-language-server directory", (not sure this one is really necessary)
+				\ "command": "full path to lua-language-server executable",
+				\ "args": ["-E", "-e", "LANG=en", "[full path of lua-language-server directory]/main.lua"],
+				\ "filetypes": ["lua"],
+				\ "rootPatterns": [".git/"]
+				\}
+				\})
 '''
 `
 
@@ -191,12 +168,9 @@ hook_add = '''
 [[plugins]]
 repo = 'vim-python/python-syntax'
 on_ft = 'python'
-hook_add = '''
-	"python lsp config
-	if dein#tap('python-syntax')
-		let g:python_highlight_all = 1
-		call coc#add_extension('coc-python')
-	endif
+hook_source = '''
+	let g:python_highlight_all = 1
+	call coc#add_extension('coc-python')
 '''
 
 [[plugins]]
@@ -216,14 +190,12 @@ on_ft = 'requirements'
 [[plugins]]
 repo = othree/html5.vim
 on_ft = 'html'
-hook_add = '''
+hook_source = '''
     let g:html5_event_handler_attributes_complete = 0
     let g:html5_rdfa_attributes_complete = 0
     let g:html5_microdata_attributes_complete = 0
     let g:html5_aria_attributes_complete = 0
-	if dein#tap(html5.vim)
-		call coc#add_extension('coc-html')
-	endif
+	call coc#add_extension('coc-html')
 '''
 `
 
@@ -231,7 +203,7 @@ hook_add = '''
 [[plugins]]
 repo = 'hail2u/vim-css3-syntax'
 on_ft = 'css'
-hook_add = '''
+hook_source = '''
 	call coc#add_extension('coc-css')
 '''
 `
@@ -254,8 +226,184 @@ on_ft = 'stylus'
 [[plugins]]
 repo = 'dart-lang/dart-vim-plugin'
 on_ft = 'dart'
-hook_add = '''
+hook_source = '''
 	call coc#add_extension('coc-flutter')
 '''
+`
+	PlugCFamily = `
+Plug 'jackguo380/vim-lsp-cxx-highlight',{'for' : [ 'c','cpp' ]}
+`
+	PlugCFamilyLsp = `
+" ccls config
+call coc#config('languageserver', {
+\ 'ccls': {
+	\ "command": "ccls",
+	\ "rootPatterns": [".ccls", "compile_commands.json", ".git/", ".hg/"],
+	\ "filetypes": ["c","cpp","objc","objcpp"],
+	\ "initializationOptions": {
+	\ "cache":{
+		\ "directory": "/tmp/ccls"
+	\ }
+	\ }
+	\ }
+	\})
+'''
+`
+	PlugR = `
+Plug 'jalvesaq/Nvim-R' ,{'for': 'R'}
+`
+	PlugRLsp = `
+call coc#add_extension('coc-R')
+`
+	PlugJavascript = `
+Plug 'pangloss/vim-javascript', {'for': [ 'javascript', 'javascriptreact' ]}
+Plug 'moll/vim-node', {'for': [ 'javascript', 'javascriptreact' ]}
+`
+	PlugJavascriptLsp = `
+let g:javascript_plugin_jsdoc = 1
+let g:javascript_plugin_flow = 1
+"javascript lsp config
+call coc#add_extension('coc-tsserver','coc-eslint','coc-prettier','coc-docthis')
+`
+
+	PlugTypescript = `
+Plug  'HerringtonDarkholme/yats.vim' , {'for' : [ 'typescript', 'typescriptreact' ]}
+`
+	PlugTypescriptLsp = `
+call coc#add_extension('coc-tsserver','coc-eslint', 'coc-prettier', 'coc-tslint-plugin' ,'coc-docthis')
+`
+
+	PlugReact = `
+Plug 'MaxMEllon/vim-jsx-pretty', {'for': [ 'javascript', 'javascriptreact', 'typescriptreact' ]}
+`
+	PlugVue = `
+Plug 'posva/vim-vue' , {'for': 'vue'}
+`
+	PlugGo = `
+Plug  'hardcoreplayers/go-nvim' , {'for' : ['go','gomod']}
+`
+	PlugGoLsp = `
+call coc#config('languageserver', {
+\ 'golang': {
+	\ "command": "gopls",
+	\ "rootPatterns": ["go.mod"],
+	\ "disableWorkspaceFolders": "true",
+	\ "filetypes": ["go"]
+	\ }
+	\})
+`
+
+	PlugRust = `
+Plug 'rust-lang/rust.vim' ,{'for' : 'rust'}
+`
+	PlugRustLsp = `
+call coc#add_extension('coc-rust-analyzer')
+`
+	PlugHaskell = `
+Plug 'neovimhaskell/haskell-vim' , {'for': 'haskell'}
+`
+
+	PlugPhp = `
+Plug 'StanAngeloff/php.vim' , {'for': 'php'}
+`
+	PlugPhpLsp = `
+call coc#config('languageserver', {
+\ 'intelephense': {
+	\ "command": "intelephense",
+	\ "args": ["--stdio"],
+	\ "filetypes": ["php"],
+	\ "initializationOptions": {
+		\ "storagePath": "/tmp/intelephense"
+	\ }
+	\ }
+	\})
+`
+	PlugRuby = `
+Plug 'vim-ruby/vim-ruby' ,{'for' : 'ruby'}
+`
+	PlugRubyLsp = `
+call coc#add_extension('coc-solargraph')
+`
+
+	PlugScala = `
+Plug 'derekwyatt/vim-scala',{'for': 'scala'}
+`
+	PlugScalaLsp = `
+call coc#add_extension('coc-metals')
+`
+	PlugShell = `
+Plug 'arzg/vim-sh', {'for': [ 'sh','zsh' ]}
+`
+	PlugShellLsp = `
+call coc#config('languageserver', {
+\ 'bash': {
+	\ "command": "bash-language-server",
+	\ "args" : ["start"],
+	\ "ignoredRootPaths": ["~"],
+	\ "filetypes": ["sh"]
+	\ }
+	\})
+`
+	PlugLua = `
+Plug 'tbastos/vim-lua',{'for': 'lua'}
+`
+	PlugLuaLsp = `
+call coc#config ('languageserver', {
+		\'lua': {
+			\ "cwd": "full path of lua-language-server directory", (not sure this one is really necessary)
+			\ "command": "full path to lua-language-server executable",
+			\ "args": ["-E", "-e", "LANG=en", "[full path of lua-language-server directory]/main.lua"],
+			\ "filetypes": ["lua"],
+			\ "rootPatterns": [".git/"]
+			\}
+			\})
+`
+
+	PlugPython = `
+Plug 'vim-python/python-syntax' ,{'for': 'python'}
+Plug 'Vimjas/vim-python-pep8-indent',{'for': 'python'}
+Plug 'vim-scripts/python_match.vim',{'for': 'python'}
+Plug 'raimon49/requirements.txt.vim',{'for': 'python'}
+`
+	PlugPythonLsp = `
+let g:python_highlight_all = 1
+call coc#add_extension('coc-python')
+`
+
+	PlugHtml = `
+Plug othree/html5.vim, {'for': 'html'}
+`
+
+	PlugHtmlLsp = `
+let g:html5_event_handler_attributes_complete = 0
+let g:html5_rdfa_attributes_complete = 0
+let g:html5_microdata_attributes_complete = 0
+let g:html5_aria_attributes_complete = 0
+call coc#add_extension('coc-html')
+'''
+`
+
+	PlugCss = `
+Plug 'hail2u/vim-css3-syntax' ,{'for': 'css'}
+`
+	PlugCssLsp = `
+call coc#add_extension('coc-css')
+`
+	PlugLess = `
+Plug 'groenewege/vim-less' ,{'for' : 'less'}
+`
+	PlugSass = `
+Plug 'cakebaker/scss-syntax.vim' ,{'for' : [ 'scss', 'sass' ]}
+`
+	PlugStylus = `
+Plug 'iloginow/vim-stylus' , {'for' : 'stylus'}
+`
+
+	PlugDart = `
+Plug  'dart-lang/dart-vim-plugin' ,{'for': 'dart'}
+`
+
+	PlugDartLsp = `
+call coc#add_extension('coc-flutter')
 `
 )
